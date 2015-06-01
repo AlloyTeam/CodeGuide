23a4ed0d3c005eed4bcd0d389b446d7a629fe9ab
Yuri4ever <398459270@qq.com>
2015-06-01 17:07:46 +0800

    变量声明示例代码更新

--- _includes/javascripts/variable_declaration.js
+++ _includes/javascripts/variable_declaration.js
@@ -1,11 +1,9 @@
 function doSomethingWithItems(items) {
     // use one var
     var value = 10,
-        result = value + 10,
-        i,
-        len;
+        result = value + 10;
 
-    for (i = 0, len = items.length; i < len; i++) {
+    for (var i = 0, len = items.length; i < len; i++) {
         result += 10;
     }
 }
